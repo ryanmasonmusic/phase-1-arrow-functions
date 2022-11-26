d9cb667300591b57f531ea2d7059fb7f8b991f10
Ryan Mason <ryanmasonmusic@gmail.com>
2022-11-26 18:27:18 -0500

    commit tree 99edd7d8fe66b5cf194e7c32c6ca7d7cf732f7f0

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000, 100);
+
+
+let square = x => x * x;
+square(2);
+
+let add = (parameter3, parameter4) => parameter3 + parameter4;
+add(3, 4);
